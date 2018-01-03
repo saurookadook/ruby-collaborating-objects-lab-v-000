5cae449c2efb6e1f0ba739f5cf87060cc357d9a3
Andy Maskiell <maskiella@gmail.com>
2018-01-03 15:31:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    self.save
+    # self.save
   end
 
   def songs
@@ -24,26 +24,26 @@ class Artist
     @@all
   end
 
-  def self.find_or_create_by_name(name)
-    find = @@all.detect {|artist_instance| name == artist_instance.name}
-    if find
-      find
-    else
-      # self.new(name)
-      new_artist = self.new(name)
-      new_artist.save
-      new_artist
-    end
-
   # def self.find_or_create_by_name(name)
-  #    find = @@all.detect do |artist_instance|
-  #     artist_name = artist_instance.name
-  #     if artist_name != nil
-  #       artist_instance
-  #     elsif artist_name == name
-  #       self.new(name)
-  #     end
+  #   find = @@all.detect {|artist_instance| name == artist_instance.name}
+  #   if find
+  #     find
+  #   else
+  #     # self.new(name)
+  #     new_artist = self.new(name)
+  #     new_artist.save
+  #     new_artist
   #   end
+
+  def self.find_or_create_by_name(name)
+     find = @@all.detect do |artist_instance|
+      artist_name = artist_instance.name
+      if artist_name != nil
+        artist_instance
+      elsif artist_name == name
+        self.new(name)
+      end
+    end
   find.save
   end
 
